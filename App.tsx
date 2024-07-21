981339ce30db77d58d12895017c83a42688fe703
Juanco <juanffo874@gmail.com>
2024-07-20 23:39:02 -0600

    Añadiendo el provider de react native paper a la raiz del proyecto

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Image } from 'react-native'
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-native-paper';
 
 import Guardados from './views/Guardados';
 import Inicio from './views/Inicio';
@@ -12,47 +13,49 @@ const Tab = createMaterialBottomTabNavigator()
 const App = () => {
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name='Inicio'
-          component={Inicio}
-          options={{
-            tabBarIcon: ({focused}) => (
-              <Image
-                source={
-                  focused ? 
-                    require('./src/img/homeNegro.png') :
-                    require('./src/img/home.png') 
-                }
-                style={{
-                  height: 22, width: 22
-                }}
-              />
-            )
-          }}
-        />
+    <Provider>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name='Inicio'
+            component={Inicio}
+            options={{
+              tabBarIcon: ({focused}) => (
+                <Image
+                  source={
+                    focused ? 
+                      require('./src/img/homeNegro.png') :
+                      require('./src/img/home.png') 
+                  }
+                  style={{
+                    height: 22, width: 22
+                  }}
+                />
+              )
+            }}
+          />
 
-        <Tab.Screen
-          name='Guardados'
-          component={Guardados}
-          options={{
-            tabBarIcon: ({focused}) => (
-              <Image
-                source={
-                  focused ? 
-                    require('./src/img/guardarNegro.png') :
-                    require('./src/img/guardar.png') 
-                }
-                style={{
-                  height: 22, width: 22
-                }}
-              />
-            )
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+          <Tab.Screen
+            name='Guardados'
+            component={Guardados}
+            options={{
+              tabBarIcon: ({focused}) => (
+                <Image
+                  source={
+                    focused ? 
+                      require('./src/img/guardarNegro.png') :
+                      require('./src/img/guardar.png') 
+                  }
+                  style={{
+                    height: 22, width: 22
+                  }}
+                />
+              )
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
