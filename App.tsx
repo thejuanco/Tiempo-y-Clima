ed46a4897455e0d8c1af69c542ce13397d8878b6
Juanco <juanffo874@gmail.com>
2024-07-22 20:08:04 -0600

    Renombrando la Tab.Screen

--- App.tsx
+++ App.tsx
@@ -36,7 +36,7 @@ const App = () => {
           />
 
           <Tab.Screen
-            name='Guardados'
+            name='Favoritos'
             component={Guardados}
             options={{
               tabBarIcon: ({focused}) => (
