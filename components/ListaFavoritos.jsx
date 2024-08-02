b780bdd0eb5db280b18d6e5433c7956311bc9bc0
Juanco <juanffo874@gmail.com>
2024-08-02 14:22:36 -0600

    Renderizando los favoritos en su pagina

--- components/ListaFavoritos.jsx
+++ components/ListaFavoritos.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, {useState, useEffect} from 'react';
+import {View, ScrollView} from 'react-native';
+import {Text} from 'react-native-paper';
 
-const ListaFavoritos = () => {
+const ListaFavoritos = ({datos}) => {
+  //Almacenando los favoritos en un array
+  const [favoritos, setFavorito] = useState([]);
+
+  console.log(datos);
+  // Añadir los datos al array de favoritos cuando cambia
+  useEffect(() => {
+    if (datos) {
+      setFavorito(prevFavoritos => [...prevFavoritos, datos]);
+    }
+  }, [datos]);
+
   return (
-    <View>ListaFavoritos</View>
-  )
-}
+    <View>
+      <ScrollView>
+        {favoritos.length === 0 ? (
+          <>
+            <Text>No hay favoritos</Text>
+          </>
+        ) : (
+          favoritos.map((item, index) => (
+            <>
+              <Text key={index}>{item.temp}</Text>
+              <Text key={index}>{item.name}</Text>
+              <Text>{item.contry}</Text>
+              <Text>{item.descriptions}</Text>
+            </>
+          ))
+        )}
+      </ScrollView>
+    </View>
+  );
+};
 
-export default ListaFavoritos
+export default ListaFavoritos;
